58212b4a5981ebee151aa43c2d7a69ab41aa8973
rebecca <rebeccccawong@gmail.com>
2024-03-23 20:46:07 -0700

    index, about, style guide, and project pages

--- script.js
+++ script.js
@@ -6,20 +6,38 @@ var doughover = document.getElementById('nav-doug');
 var dougshow = document.getElementById('index-doug');
 var micrositehover = document.getElementById('nav-microsite');
 var micrositeshow = document.getElementById('index-microsite');
+var rebrandinghover = document.getElementById('nav-rebranding');
+var rebrandingshow = document.getElementById('index-rebranding');
 
+// INDEX DOUGLAS ROYALS
 doughover.addEventListener("mouseenter", function(event) {
   dougshow.style.display = 'block';
+  console.log('Douglas Royals content showing');
 });
 
 doughover.addEventListener("mouseout", function(event) {
   dougshow.style.display = 'none';
+  console.log('Douglas Royals content not showing');
 });
 
-
+// INDEX MICROSITE
 micrositehover.addEventListener("mouseenter", function(event) {
   micrositeshow.style.display = 'block';
+  console.log('Microsite content showing');
 });
 
 micrositehover.addEventListener("mouseout", function(event) {
   micrositeshow.style.display = 'none';
+  console.log('Microsite content not showing');
+});
+
+// INDEX REBRANDING
+rebrandinghover.addEventListener("mouseenter", function(event) {
+  rebrandingshow.style.display = 'block';
+  console.log('Rebranding content showing');
+});
+
+rebrandinghover.addEventListener("mouseout", function(event) {
+  rebrandingshow.style.display = 'none';
+  console.log('Rebranding content not showing');
 });
